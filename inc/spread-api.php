0820e45a1f9086ab0ffb8cb806abaf63b633396a
Simon G.Proudfoot <simonproudfoot1980@gmail.com>
2023-05-04 10:17:55 +0100

    file import working

--- inc/spread-api.php
+++ inc/spread-api.php
@@ -1,16 +1,65 @@
 <?php
 $apiKey = 'dd30b4db-8cd6-4fb8-86b3-e680984b9e18';
+function get_image_from_api($url)
+{
+    global $apiKey;
+    $ch = curl_init();
+
+    // Set cURL options
+    curl_setopt_array($ch, array(
+        CURLOPT_URL => $url,
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_HTTPHEADER => array(
+            'Authorization: SprdAuth apiKey="' . $apiKey . '"',
+            'User-Agent: greenwich/1.0',
+            'Access-Control-Allow-Origin: *',
+        ),
+    ));
+
+    // Execute the cURL request
+    $response = curl_exec($ch);
+
+    // Check if the request was successful
+    if (curl_errno($ch)) {
+        echo 'Error: ' . curl_error($ch);
+        return null;
+    }
+
+    // Get the response code
+    $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    // Check if the response code is OK
+    if ($http_code != 200) {
+        echo 'Error: API returned HTTP status code ' . $http_code;
+        return null;
+    }
+
+    // Get the content type header
+    $content_type = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+
+    // Check if the content type is an image type
+    if (strpos($content_type, 'image/') !== 0) {
+        echo 'Error: API did not return an image';
+        return null;
+    }
+
+    // Return the image data
+    $blob = $response;
+    return file_get_contents($blob);
+}
+
+
 
 function get_spreadshirt_data($endPoint, $postData, $page)
 {
     global $apiKey;
-    $pageOffset = $page != null? '&page='.$page : '';
+    $pageOffset = $page != null ? '&page=' . $page : '';
     $nextParam = '?';
     if (preg_match('/\?/', $endPoint)) {
         $nextParam = '&';
     }
     $curl = curl_init();
-    $url = 'https://api.spreadshirt.net/api/v1/shops/101082106/' . $endPoint . $nextParam . 'mediaType=json&fullData=true'.$pageOffset;
+    $url = 'https://api.spreadshirt.net/api/v1/shops/101082106/' . $endPoint . $nextParam . 'mediaType=json&fullData=true' . $pageOffset;
     curl_setopt_array($curl, array(
         CURLOPT_URL => $url,
         CURLOPT_RETURNTRANSFER => true,
